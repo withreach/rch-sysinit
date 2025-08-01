1cae64fc96e92de68c96cd839691055d509cd8d8
kedwards <kedwards@kevinedwards.ca>
2025-08-01 11:38:49 -0600

    inital commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Linux System Initialization
+
+An opinionated script to initialize your Linux system for use with Reach Ltd.
+
+## Distros
+
+This release has been tested on the following distributions, and should work on all distros based from:
+
+- Arch
+- Debian
+- Fedora
+- Redhat
+- Ubuntu
+
+## Quick Start
+
+Open a terminal and run one of the following:
+
+```bash
+wget -O - https://bit.ly/_sysinit | bash
+```
+
+```bash
+wget -O - https://links.kevinedwards.ca/sysinit | bash
+```
+
+Alternatively if you want to audit the script clone the repo, and run the install.sh script:
+
+```bash
+git clone https://github.com/withreach/sysinit.git
+cd sysinit
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+script_dir="$(dirname "$(realpath "$0")")"
+sysinit_path="$HOME/sysinit"
+packages="curl git gpg"
+repository=https://github.com/withreach/sysinit.git
+
+trap cleanup ERR EXIT
+
+cleanup() {
+  if command -v deactivate &>/dev/null; then
+    deactivate
+  fi
+}
+
+function getPackageManager() {
+  local pm
+
+  declare -A osInfo
+  osInfo['/etc/redhat-release']=yum
+  osInfo['/etc/arch-release']=pacman
+  osInfo['/etc/debian_version']=apt-get
+
+  for f in "${!osInfo[@]}"; do
+    if [[ -f $f ]]; then
+      pm="${osInfo[$f]}"
+    fi
+  done
+  echo "$pm"
+}
+
+case $(getPackageManager) in
+"apt-get")
+  sudo sh -c "apt-get update; apt-get upgrade -y; apt-get install $packages -y; apt autoremove -y"
+  ;;
+"pacman")
+  sudo sh -c "pacman -Syu; echo 'yes' | pacman -S --noconfirm $packages"
+  ;;
+"yum")
+  sudo ish -c "yum update; yum install -y $packages"
+  ;;
+*)
+  echo "Unsupported package manager"
+  exit 1
+  ;;
+esac
+
+curl https://mise.run | sh
+
+# Add mise to PATH and activate it
+export PATH="$HOME/.local/bin:$PATH"
+echo "eval \"\$($HOME/.local/bin/mise activate bash)\"" >>~/.bashrc
+
+# Source the mise activation directly instead of relying on .bashrc
+eval "$($HOME/.local/bin/mise activate bash)"
+
+# Now mise should be available
+mise use --global uv
+
+#if [ ! -d "$HOME/.venv/sysinit" ]; then
+uv venv --clear "$HOME/.venv/sysinit"
+#fi
+source "$HOME/.venv/sysinit/bin/activate"
+
+# if [ -d "${sysinit_path}" ] && [ -d "${sysinit_path}/.git" ]; then
+#   git -C "${sysinit_path}" pull
+# else
+#   git clone -b main --single-branch $repository "$sysinit_path"
+# fi
+
+uv pip install -r "$script_dir/requirements.txt"
+
+#cd "${sysinit_path}" || exit 1
+#mise trust -a
+ansible-playbook playbook.yml -K
